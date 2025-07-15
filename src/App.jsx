df5908ed8e510b3c6bbde84cbefee2b03f54de1e
Mateus Cibelli de Oliveira <mateuscibelli@hotmail.com>
2025-07-15 18:07:10 -0300

    build: project exclusive

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className='text-blue-500 font-custom-graphik font-normal'>Teste Tailwind Graphik normal!</h1>
+      <h2 className='text-blue-500 font-custom-graphik font-bold'>Teste Tailwind Graphik bold!</h2>
+    </>
+  )
+}
+
+export default App
