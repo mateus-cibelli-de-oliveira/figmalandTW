40750df90a73983fe9701dfca9ba736e9992a7b5
Mateus Cibelli de Oliveira <mateuscibelli@hotmail.com>
2025-07-18 16:36:10 -0300

    testimonials project integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { CalendarFeature } from './components/CalendarFeature'
+import { Testimonials } from './components/Testimonials'
 
 function App() {
   return (
     <div className='bg-dark-background'>
+        <Testimonials />
         <CalendarFeature />
     </div>
   )
---
+++ src/components/Testimonials/index.jsx
@@ -0,0 +1,32 @@
+import { Heading } from '../Heading'
+import { Button } from '../button'
+import IbmLogo from '../../assets/images/brands/ibm-logo.png'
+import TestimonialPerson from '../../assets/images/testimonials/testimonials-2.png'
+
+export const Testimonials = () => {
+    return (
+        <section className='bg-white'>
+            <div className='w-full max-w-[360px] md:max-w-[900px] mx-auto flex flex-col justify-center items-center gap-[80px] md:px-5 xl:px-0'>
+                <Heading type='h2'>
+                    Testimonials
+                </Heading>
+                <div className='flex flex-col py-4 justify-center items-center gap-11 md:gap-12'>
+                    <img src={IbmLogo} />
+                    <p className='text-center text-sm text-secondary-text font-bold w-full max-w-[280px] md:max-w-none md:text-2xl md:font-normal'>
+                        Most calendars are designed for teams.
+                        Slate is designed for freelancers who want a
+                        simple way to plan their schedule.
+                    </p>
+                    <div className='flex gap-3 justify-center items-center'>
+                        <img src={TestimonialPerson} />
+                        <div>
+                            <p className='text-xs text-secondary-text font-bold'>Organize across</p>
+                            <p className='text-ms text-black'>Ui designer</p>
+                        </div>
+                    </div>
+                </div>
+                <Button className='w-full max-w-[280px]'>More Testimonials</Button>
+            </div>
+        </section>
+    )
+}
